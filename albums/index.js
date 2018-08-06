37e54547a37257b9c59251ff44e013663ec949a8
Shailendra Acharya <acharyashailendra1@gmail.com>
2018-08-07 00:32:18 +0530

    added different style properties and images

--- albums/index.js
+++ albums/index.js
@@ -9,7 +9,7 @@ import AlbumList from './src/components/AlbumList';
 // Create a component 
 const App =()=> (
 /* This is a comment */
-   <View>
+   <View style={{flex:1}}>
     <Header headerText={'Albums!'}/>
     <AlbumList/>
     </View>
