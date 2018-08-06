7bde53040a478e74a8bbd7c2e330d84773181c8c
Shailendra Acharya <acharyashailendra1@gmail.com>
2018-08-06 23:42:01 +0530

    added carsection

--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {View} from 'react-native';
 
-const card = (props)=>{
+const Card = (props)=>{
 
     return(
         <View style={styles.containerStyle}>
@@ -24,10 +24,10 @@ const styles ={
         shadowOpacity : 0.1,
         shadowRadius : 2,
         elevation : 1,
-        marginleft :5,
-        marginright: 5,
-        margintop:10
+        marginLeft :5,
+        marginRight: 5,
+        marginTop:10
     }
-}
+};
 
 export default Card;
